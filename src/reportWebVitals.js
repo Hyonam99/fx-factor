0bcb4c3c675b26b71a0cea073229f62f96f4a9df
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-08-05 13:41:40 +0100

    eslint clean up7

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable max-len */
 const reportWebVitals = (onPerfEntry) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
