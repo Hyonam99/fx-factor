5fd382dd2831a73752114de0548bcdcfb00fea08
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-08-05 01:15:07 +0100

    strap up project with typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="title">Welcome to the FX Factor</h1>
+        </div>
+    )
+}
+
+export default App;
